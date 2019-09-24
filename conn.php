1d418cd0ff49db7beba8db1190e95259331f7fb7
wufengwind <48611943+wufengwind@users.noreply.github.com>
2019-09-24 20:37:29 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-$mysqli = mysqli_connect('47.106.132.48', 'root', 'wf1105yz0418', 'lingting');
+$mysqli = mysqli_connect('数据库ip地址', '用户名', '数据库密码', '数据库名');
 $mysqli->set_charset("UTF8");
 ?>
